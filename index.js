81ed1958894ca203bda72c692db6d727cf3fcd2d
zaquari_andl <zaquariandl043@gmail.com>
2023-11-14 19:12:34 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    customerName = 'BOB'
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob" 
+}
+
+function overwriteBestCustomer() {
+    if (bestCustomer === "not bob") {
+        bestCustomer = "maybe bob"
+    }    
+}
+
+const leastFavoriteCustomer = 'something'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "something else"
+}
+
+leastFavoriteCustomer()
